b035965ba7d98056d6940dac350bfd9511b9c137
sundarok <38673525+sundarok@users.noreply.github.com>
2025-03-18 16:23:19 -0700

    feat: Added logic to compare actual and expected BOM file
    Signed-off-by: sundarok <38673525+sundarok@users.noreply.github.com>

--- cmd/compare.go
+++ cmd/compare.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"encoding/json"
-	"github.com/golang-collections/collections/set"
+	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"os"
@@ -14,14 +18,35 @@ var (
 	actualKBOMPath   string
 )
 
+var (
+	red   = "\033[31m"
+	green = "\033[32m"
+	reset = "\033[0m"
+)
+
 const APPLICATION = "application"
 const CONTAINER = "container"
-const BOM_PROPERTY_NAMESPACE = "clx:k8s:componentNamespace"
+
+const BOM_PROPERTY_NAME = "cdx:k8s:component:name"
+const BOM_PROPERTY_CONTAINER_NAMESPACE = "clx:k8s:componentNamespace"
+
 const ComponentType = "component-type"
 
 const ECR_REGEX = `(\d+)\.dkr\.ecr\.[a-z0-9-]+\.amazonaws\.com`
 
 var ecrRegex = regexp.MustCompile(ECR_REGEX)
+
+type ComponentData struct {
+	Name       string
+	Type       string
+	Properties []ComparisonProperty
+}
+
+type ComparisonProperty struct {
+	PropertyName string
+	Actual       string
+	Expected     string
+}
 
 var CompareCmd = &cobra.Command{
 	Use:   "compare",
@@ -40,6 +65,7 @@ func init() {
 }
 
 func compareKBOM(cmd *cobra.Command, _ []string) error {
+	var comparisonError = false
 
 	expected, err := os.ReadFile(expectedKBOMPath)
 	if err != nil {
@@ -63,10 +89,33 @@ func compareKBOM(cmd *cobra.Command, _ []string) error {
 	actualContainerMap := extractBOMToMap(actualStruct, CONTAINER)
 	actualApplicationMap := extractBOMToMap(actualStruct, APPLICATION)
 
-	//for key, value := range expectedContainerMap {
-	//	fmt.Printf("- %s: %d\n", key, value)
-	//}
+	// Compare Golden and actual KBOM, return response as Error and Warning
+	containerMismatchWarning, containerMismatchError := compareKBOMData(expectedContainerMap, actualContainerMap)
+	applicationMismatchWarning, applicationMismatchError := compareKBOMData(expectedApplicationMap, actualApplicationMap)
 
+	// Print the error in table format
+	if containerMismatchError != nil || applicationMismatchError != nil {
+		fmt.Println("ERROR!")
+		fmt.Println("Note: An error is when there is a mismatch in the actual cluster vs Golden KBOM")
+		printMismatches(containerMismatchError, CONTAINER, false)
+		printMismatches(applicationMismatchError, APPLICATION, false)
+
+		comparisonError = true
+	}
+
+	// Print the warning in table format
+	if containerMismatchWarning != nil || applicationMismatchWarning != nil {
+		fmt.Println("\nWARNING!")
+		fmt.Println("Note: A warning is when additional containers or applications are present in actual cluster or in Golden KBOM")
+		printMismatches(containerMismatchWarning, CONTAINER, true)
+		printMismatches(applicationMismatchWarning, APPLICATION, true)
+	}
+
+	if comparisonError == true {
+		fmt.Errorf("Error: Found mismatches between Golden KBOM and actual cluster KBOM")
+	} else {
+		fmt.Println("No errors found during KBOM comparison.")
+	}
 	return nil
 }
 
@@ -87,9 +136,112 @@ func extractBOMToMap(data *unstructured.Unstructured, dataType string) map[strin
 				itemValue := props.(map[string]interface{})["value"].(string)
 				innerContainerMap[itemKey] = itemValue
 			}
+			innerContainerMap[BOM_PROPERTY_NAME] = component["name"].(string)
 			dataMap[purl] = innerContainerMap
 		}
 	}
 
 	return dataMap
+}
+
+func compareKBOMData(expected map[string]map[string]string, actual map[string]map[string]string) ([]ComponentData, []ComponentData) {
+	var warnMismatching []ComponentData
+	var errorMismatching []ComponentData
+
+	// Check if the expected component from Golden KBOM is present in the actual cluster KBOM
+	for name, expectedProps := range expected {
+		actualProps, found := actual[name]
+
+		friendlyName := expectedProps[BOM_PROPERTY_NAME]
+		//kbomType := expectedProps[BOM_PROPERTY_TYPE]
+		namespace := expectedProps[BOM_PROPERTY_CONTAINER_NAMESPACE]
+
+		// If the component is not present in the actual cluster, we consider it as a warning
+		if !found {
+			comparisonProp := []ComparisonProperty{{PropertyName: "", Expected: "Exists",
+				Actual: "Missing"}}
+			mismatch := ComponentData{Name: /*name*/ namespace + "/" + friendlyName, Type: expectedProps[ComponentType], Properties: comparisonProp}
+			warnMismatching = append(warnMismatching, mismatch)
+		} else {
+			propertyDiff := compareMaps(expectedProps, actualProps)
+			// If the component is present in the actual cluster, but with different properties, we consider it as an error
+			if propertyDiff != nil {
+				mismatch := ComponentData{Name: /*name*/ namespace + "/" + friendlyName, Type: actualProps[ComponentType], Properties: propertyDiff}
+				errorMismatching = append(errorMismatching, mismatch)
+			}
+		}
+		// Compare the properties of the expected and actual components
+	}
+	return warnMismatching, errorMismatching
+}
+
+func compareMaps(expected, actual map[string]string) []ComparisonProperty {
+	var differences []ComparisonProperty
+
+	for key, val1 := range expected {
+		if val2, exists := actual[key]; !exists || ecrRegex.ReplaceAllString(val1, "*") != ecrRegex.ReplaceAllString(val2, "*") {
+			comparisonProp := ComparisonProperty{PropertyName: key, Expected: val1, Actual: val2}
+			differences = append(differences, comparisonProp)
+		}
+	}
+
+	// Check for keys in actual that are not in expected
+	for key, val2 := range actual {
+		if _, exists := expected[key]; !exists {
+			comparisonProp := ComparisonProperty{PropertyName: key, Expected: "Null", Actual: val2}
+			differences = append(differences, comparisonProp)
+		}
+	}
+
+	return differences
+}
+
+func printMismatches(errors []ComponentData, componentType string, skipPropertyName bool) {
+	if len(errors) <= 0 {
+		return
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	// Skip setting 'PropertyName' column for KBOM warning
+	header := table.Row{"Type", "Name"}
+	if !skipPropertyName {
+		header = append(header, "Property Name")
+	}
+	header = append(header, "Golden", getClusterName())
+	t.AppendHeader(header)
+
+	for _, differences := range errors {
+		name, ctype := text.WrapHard(differences.Name, 50), componentType
+		for _, values := range differences.Properties {
+			row := table.Row{ctype, name}
+			// Skip setting 'PropertyName' values for KBOM warning
+			if !skipPropertyName {
+				row = append(row, values.PropertyName)
+			}
+			row = append(row, text.WrapHard(getColoredText(values.Expected, green), 50), text.WrapHard(getColoredText(values.Actual, red), 50))
+			t.AppendRows([]table.Row{row})
+			name, ctype = "", ""
+		}
+		t.AppendSeparator()
+	}
+	t.Render()
+}
+
+func getColoredText(text string, color string) string {
+	// Override the default color for KBOM warning messages
+	if strings.Contains(text, "Missing") {
+		color = red
+	} else if strings.Contains(text, "Exists") {
+		color = green
+	}
+	return fmt.Sprint(color, text, reset)
+}
+func getClusterName() (clusterName string) {
+	clusterName = os.Getenv("CLUSTER_NAME")
+	if clusterName == "" {
+		clusterName = "Current Cluster"
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -6,11 +6,11 @@ require (
 	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
 	github.com/google/go-containerregistry v0.20.3
 	github.com/google/uuid v1.6.0
+	github.com/jedib0t/go-pretty/v6 v6.6.7
 	github.com/onsi/ginkgo/v2 v2.22.1
 	github.com/onsi/gomega v1.36.2
 	github.com/spf13/cobra v1.8.1
-	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.9.0
+	github.com/stretchr/testify v1.10.0
 	k8s.io/api v0.32.1
 	k8s.io/apimachinery v0.32.1
 	k8s.io/client-go v0.32.1
@@ -36,12 +36,15 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/oauth2 v0.25.0 // indirect
